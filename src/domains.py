093ba69b41df81e26594c425e0585cdf48dbc4ff
duc2ht <30986754+duc2ht@users.noreply.github.com>
2024-07-21 15:33:18 +0700

    Update domains.py

--- src/domains.py
+++ src/domains.py
@@ -1,6 +1,6 @@
 import os
 import http.client
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 from configparser import ConfigParser
 from src import info, convert, silent_error
 
@@ -30,17 +30,16 @@ class DomainConverter:
                     url.strip() for url in file if not url.startswith("#") and url.strip()
                 ]
         return urls
-    
+
     def read_urls_from_env(self, env_var):
         urls = os.getenv(env_var, "")
-        return [
-            url.strip() for url in urls.split() if url.strip()
-        ]
+        return [url.strip() for url in urls.split() if url.strip()]
 
     def read_urls(self, env_var):
-        file_path = self.env_file_map[env_var]
-        urls = self.read_urls_from_file(file_path)
-        urls += self.read_urls_from_env(env_var)
+        urls = self.read_urls_from_env(env_var)
+        if not urls:
+            file_path = self.env_file_map[env_var]
+            urls = self.read_urls_from_file(file_path)
         return urls
 
     def download_file(self, url):
